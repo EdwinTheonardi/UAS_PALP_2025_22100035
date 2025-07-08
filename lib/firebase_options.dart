4beaacf320e930f8785e9d693b86a1a7272d6995
EdwinTheonardi <edwintheonardi88@gmail.com>
2025-07-08 08:27:00 +0700

    first commit

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyAd8MOKjpeF8klkmoMwASdqmg2TKgT0Yi8', 
+      appId: '1:534298912541:android:7b92fc6539e60737a423f4', 
+      messagingSenderId: '534298912541', 
+      projectId: 'uas-palp-3f97b', 
+      storageBucket: 'uas-palp-3f97b.firebasestorage.app', 
+    );
+  }
+}
